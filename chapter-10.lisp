e631edf9ea00c21a8ecf2e5f98a7a5973b9dd0e9
denlab <denis.labaye@gmail.com>
2011-01-20 21:45:34 +0100

    Added warning about bad style of the first add method.

--- chapter-10.lisp
+++ chapter-10.lisp
@@ -14,6 +14,13 @@
 ;; > (add '(a b) '(c d))
 ;; (A B C D)
 
+;; WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!
+;; 
+;; This is not how we should write generic functions in lisp !
+;; Should be done using type dispatch using defmethod !
+;; 
+;; WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!
+
 (defun add (a b)
      (cond ((and (numberp a) (numberp b)) (+ a b))
            ((and (listp a) (listp b)) (append a b))))
