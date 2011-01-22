e5e0029c68643ee02288bec6903a07c14b73224c
denlab <denis.labaye@gmail.com>
2011-01-22 21:41:49 +0100

    Started chapter 10, and started ORC BATTLE!

--- chapter-10.lisp
+++ chapter-10.lisp
@@ -25,3 +25,64 @@
      (cond ((and (numberp a) (numberp b)) (+ a b))
            ((and (listp a) (listp b)) (append a b))))
 
+;; ----------------------------------------------------------------------------
+;; (add)
+;; -----------------------------------------------------------------------------
+
+;; Like add-bad-style but with a good style (defmethod)
+
+(defmethod add ((a number) (b number))
+  (+ a b))
+
+(defmethod add ((a list) (b list))
+  (append a b))
+
+;; ----------------------------------------------------------------------------
+;; ORC BATTLE !
+;; -----------------------------------------------------------------------------
+
+(defparameter *player-health* nil)
+(defparameter *player-agility* nil)
+(defparameter *player-strength* nil)
+
+(defparameter *monsters* nil)
+(defparameter *monster-builders* nil)
+(defparameter *monster-num* 12)
+
+;; ----------------------------------------------------------------------------
+;; (orc-battle)
+;; -----------------------------------------------------------------------------
+
+;; TODO
+
+;; ----------------------------------------------------------------------------
+;; (game-loop)
+;; -----------------------------------------------------------------------------
+
+;; TODO
+
+;; ----------------------------------------------------------------------------
+;; Player Management Function
+;; -----------------------------------------------------------------------------
+
+(defun init-player ()
+    (setf *player-health* 30)
+    (setf *player-agility* 30)
+    (setf *player-strength* 30))
+
+(defun player-dead ()
+    (<= *player-health* 0))
+
+(defun show-player ()
+  (fresh-line)
+  (princ "You are a valiant knight with a health of ")
+  (princ *player-health*)
+  (princ ", an agility of ")
+  (princ *player-agility*)
+  (princ ", and a strength of ")
+  (princ *player-strength*))
+
+;; ----------------------------------------------------------------------------
+;; (player-attack)
+;; -----------------------------------------------------------------------------
+
